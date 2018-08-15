b39aa53cf1eea1caecc17235fb828eadccfccb7f
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-15 08:05:18 -0500

    Styling the Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,10 +3,31 @@ import {Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const Button = (props) => {
     return (
-        <TouchableOpacity>
-            <Text>Click to Buy</Text>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>Click to Buy</Text>
         </TouchableOpacity>
     )
 };
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5,
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    text: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+    }
+});
 
 export default Button;
